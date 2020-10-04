6fad0c40e5b4398ba52c7870976584ceff6a65c9
Chris Talkington <chris@talkingtontech.com>
2020-10-04 10:45:48 -0500

    Update exceptions.py

--- pycfdns/exceptions.py
+++ pycfdns/exceptions.py
@@ -5,9 +5,14 @@ class CloudflareException(Exception):
     """Base exception class for pycfdns."""
 
 
+class CloudflareAuthenticationException(CloudflareException):
+    """Error to indicate we cant authenticate against API."""
+
+
 class CloudflareConnectionException(CloudflareException):
     """Error to indicate we cant connect to API."""
 
 
-class CloudflareAuthenticationException(CloudflareException):
-    """Error to indicate we cant authenticate against API."""
+class CloudflareZoneException(CloudflareException):
+    """Error to indicate we couldnt find zone via API."""
+

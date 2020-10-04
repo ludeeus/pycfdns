d32b6a31571b837b11275c324e72d69e4d44774e
Chris Talkington <chris@talkingtontech.com>
2020-10-04 10:10:44 -0500

    add more specific exceptions

--- pycfdns/exceptions.py
+++ pycfdns/exceptions.py
@@ -3,3 +3,11 @@
 
 class CloudflareException(Exception):
     """Base exception class for pycfdns."""
+
+
+class CloudflareConnectionException(CloudflareException):
+    """Error to indicate we cant connect to API."""
+
+
+class CloudflareAuthenticationException(CloudflareException):
+    """Error to indicate we cant authenticate against API."""

3f8e343dbf47326e0c84cc33cd1b4e08cb635c71
ludeeus <ludeeus@ludeeus.dev>
2022-12-21 10:18:11 +0000

    Add missing types and docstrings to models

--- pycfdns/models.py
+++ pycfdns/models.py
@@ -1,38 +1,56 @@
 """pycfdns models."""
-# pylint: disable=missing-docstring
+from __future__ import annotations
+
 from typing import TypedDict
 
 
 class CFRecord:
-    """CFRecord."""
+    """CFRecord represents a Cloudflare DNS record."""
 
-    def __init__(self, record):
-        """Initialize."""
+    def __init__(self, record: dict[str, str]) -> None:
+        """Initialize the CFRecord object with a dictionary representation of the Cloudflare DNS record.
+
+        Args:
+            record (Dict[str, str]): A dictionary representation of the Cloudflare DNS record.
+        """
         self.record = record
 
     @property
-    def record_id(self):
+    def record_id(self) -> str | None:
+        """Return the unique identifier for the Cloudflare DNS record."""
         return self.record.get("id")
 
     @property
-    def record_type(self):
+    def record_type(self) -> str | None:
+        """Return the type of the Cloudflare DNS record (e.g. A, AAAA, CNAME, etc)."""
         return self.record.get("type")
 
     @property
-    def record_name(self):
+    def record_name(self) -> str | None:
+        """Return the name of the Cloudflare DNS record (e.g. the domain name)."""
         return self.record.get("name")
 
     @property
-    def record_proxied(self):
+    def record_proxied(self) -> bool | None:
+        """Return a boolean value indicating whether the Cloudflare DNS record is proxied or not."""
         return self.record.get("proxied")
 
     @property
-    def record_content(self):
+    def record_content(self) -> str | None:
+        """Return the content of the Cloudflare DNS record (e.g. the IP address for an A record)."""
         return self.record.get("content")
 
 
 class DNSRecord(TypedDict):
-    """Dictionary representation of a DNS record"""
+    """Dictionary representation of a DNS record
+
+    Attributes:
+        content (str): The content of the DNS record.
+        id (str): The unique identifier for the DNS record.
+        name (str): The name of the DNS record.
+        proxied (bool): Whether the DNS record is proxied or not.
+        type (str): The type of the DNS record (e.g. A, AAAA, CNAME, etc).
+    """
 
     content: str
     id: str

d8e039abd85db23827d130fb50d003c0d2bea2a3
ludeeus <joasoe@gmail.com>
2023-10-12 20:07:24 +0000

    format

--- example.py
+++ example.py
@@ -5,11 +5,16 @@ import asyncio
 import aiohttp
 from pycfdns import Client
 
+
 async def example():
     """Example usage of pycfdns."""
     async with aiohttp.ClientSession() as client_session:
-        client = Client(api_token=os.getenv("CF_API_TOKEN"), client_session=client_session)
+        client = Client(
+            api_token=os.getenv("CF_API_TOKEN"),
+            client_session=client_session,
+        )
         zones = await client.list_zones()
         print(zones)
+
 
 asyncio.get_event_loop().run_until_complete(example())
